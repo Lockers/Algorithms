e114faab1b4a9627be812d75faa7bd77f209b67b
Matt Locklin <mattlocklin@live.co.uk>
2020-04-16 12:52:18 +0100

    Finished-recipe

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 
 import math
+import time
 
 def recipe_batches(recipe, ingredients):
-  pass 
+      batches = 0
+      lista = []
+      if len(recipe) > len(ingredients):
+            return batches
+      for i in recipe:
+          lista.append(int(ingredients[i] / recipe[i]))
+      return min(lista)
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 132, 'butter': 52, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
